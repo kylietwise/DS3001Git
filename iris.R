66d4c7e2b139ec56dc8fe23a1dc48c13ea49f020
Your Name <emmamurphyberlin@gmail.com>
2021-09-08 15:42:58 -0400

    readjusted formatting

--- iris.R
+++ iris.R
@@ -3,12 +3,14 @@ iris_data <- data.frame(iris)
 
 # data viz - maddie
 library(ggplot2)
+ggplot(iris_data, aes(x=Sepal.Length, y=Sepal.Width)) + geom_boxplot()
+
+# labels - claire
+library(ggplot2)
 ggplot(iris_data, aes(x=Sepal.Length, y=Sepal.Width)) + geom_boxplot() + xlab("Sepal Length") + ylab("Sepal Width")
 
 # labels - emma
 library(ggplot2)
 ggplot(iris_data, aes(x=Sepal.Length, y=Sepal.Width)) + geom_boxplot() + 
-  xlab("Sepal Length") + ylab("Sepal Width") + theme_void()
-
-# title - claire
+  xlab("Sepal Length") + ylab("Sepal Width") + ggtitle("Sepal Width versus Sepal Length")
 
