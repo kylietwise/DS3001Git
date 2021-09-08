4d6712404afcd988cdee729fa023b399177e0ba7
Your Name <emmamurphyberlin@gmail.com>
2021-09-08 15:45:37 -0400

    formatting

--- iris.R
+++ iris.R
@@ -1,7 +1,7 @@
 # loaded data - kylie
 iris_data <- data.frame(iris)
 
-# data viz - maddie
+# data viz - maddie -
 library(ggplot2)
 ggplot(iris_data, aes(x=Sepal.Length, y=Sepal.Width)) + geom_boxplot()
 
