5cfac5aea447fd9ad125fab55c2c30e81636de97
Your Name <emmamurphyberlin@gmail.com>
2021-09-08 15:39:46 -0400

    added theme

--- iris.R
+++ iris.R
@@ -5,7 +5,10 @@ iris_data <- data.frame(iris)
 library(ggplot2)
 ggplot(iris_data, aes(x=Sepal.Length, y=Sepal.Width)) + geom_boxplot() + xlab("Sepal Length") + ylab("Sepal Width")
 
-# labels - claire
+# labels - emma
+library(ggplot2)
+ggplot(iris_data, aes(x=Sepal.Length, y=Sepal.Width)) + geom_boxplot() + 
+  xlab("Sepal Length") + ylab("Sepal Width") + theme_void()
 
+# title - claire
 
-# title - emma
